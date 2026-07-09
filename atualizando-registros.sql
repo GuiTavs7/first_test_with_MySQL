f8018ce3188ae6007fef79014b56facaeb965395
GuiTavs7 <7tavs7@gmail.com>
2026-07-09 00:25:43 -0300

    update sql

---
+++ atualizando-registros.sql
@@ -0,0 +1,7 @@
+/* Sempre usar o comando UPDATE para atualizar registros de uma tabela */
+
+/* Alterando o nome de um usuário com id específico */
+
+UPDATE usuarios SET nome = 'Paulo' WHERE id = 1;
+
+UPDATE usuarios SET nome = 'Nome de Teste' WHERE nome = 'Nome Antigo';
